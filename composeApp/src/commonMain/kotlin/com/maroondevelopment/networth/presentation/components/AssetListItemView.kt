7f242da8817585a4895dd507ca91e3045eb66236
Ben George <bgeorge@midwesttape.com>
2026-02-28 22:20:28 -0500

    Initial commit

--- composeApp/src/commonMain/kotlin/com/maroondevelopment/networth/presentation/components/AssetListItemView.kt
+++ composeApp/src/commonMain/kotlin/com/maroondevelopment/networth/presentation/components/AssetListItemView.kt
@@ -5,11 +5,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.maroondevelopment.networth.domain.entity.Asset
@@ -29,10 +31,13 @@ fun AssetListItemView(asset: Asset) {
         Text(
             text = asset.holding.name,
             fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.primary
         )
 
         Text(
-            text = $$$"$${asset.value}",
+            text = "$${asset.value}",
+            color = MaterialTheme.colorScheme.secondary
         )
     }
 
